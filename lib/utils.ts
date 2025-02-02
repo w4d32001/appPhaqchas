3dcfef30f960fcf52c76fc1878832d9f19a4ef81
w4d32001 <freedom01022001@gmail.com>
2025-02-01 19:36:21 -0500

    a

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const apiUrl = "https://api.dev.phaqchas.com/public/api"
-export const baseUrl = "https://api.dev.phaqchas.com/"
+export const baseUrl = "https://api.dev.phaqchas.com/public/"
 
 // export const apiUrl = "http://127.0.0.1:8000/api"
 // export const baseUrl = "http://127.0.0.1:8000/"
